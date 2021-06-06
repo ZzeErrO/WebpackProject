a3efac0d96c63080a47b88a0db168320214664ed
prashikjaware <prashikjaware123@gmail.com>
2021-06-06 15:42:17 +0530

    [Prashik]Added: Simple Re-rendering of component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Practice from './pages/Practice.jsx';
+
+setInterval(tick,1000);
+
+function tick(){
 const HelloWorld = () => {
     return (
         <h1>
-            Hello World
+            Hello World!!
+            <Practice/>
         </h1>
     );
 }
 
 ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+}
---
+++ src/pages/Practice.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Practice() {
+    return (
+        <div>
+            <h6>It is {new Date().toLocaleTimeString()}.</h6>
+        </div>
+    )
+}
