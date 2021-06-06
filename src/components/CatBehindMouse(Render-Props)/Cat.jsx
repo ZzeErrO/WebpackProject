2329d020ba1db0078c021bdceb19f73c2744c0a9
prashikjaware <prashikjaware123@gmail.com>
2021-06-06 18:07:25 +0530

    [Prashik]Added: Simple Example for Profiler

--- src/components/CatBehindMouse(Render-Props)/Cat.jsx
+++ src/components/CatBehindMouse(Render-Props)/Cat.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import Cat from '../../assets/Cat.jpg'
+import Cats from '../../assets/Cat.jpg'
+
+import './Cat.css';
 
 export default class Cat extends Component {
     render() {
         const mouse = this.props.mouse;
         return (
-          <img src={Cat} style={{ position: 'absolute', left: mouse.x, top: mouse.y }} alt = "Cat"/>
+          <img src={Cats} style={{ position: 'absolute', left: mouse.x, top: mouse.y }} alt = "Cat"/>
         );
       }
 }
--- src/pages/Practice.jsx
+++ src/pages/Practice.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { Profiler } from 'react'
 import MouseTracker from '../components/CatBehindMouse(Render-Props)/MouseTracker.jsx'
 
+
 export default function Practice() {
+
+    const onRenderCallback=(
+        id, // the "id" prop of the Profiler tree that has just committed
+        phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+        actualDuration, // time spent rendering the committed update
+        baseDuration, // estimated time to render the entire subtree without memoization
+        startTime, // when React began rendering this update
+        commitTime, // when React committed this update
+        interactions // the Set of interactions belonging to this update
+      )=> {
+        // Aggregate or log render timings...
+        console.log('Id is: ' + id + ' Phase is: ' + phase);
+        console.log("Actual duration is: " + actualDuration + " Base Duration is: " + baseDuration);
+      }
+
     return (
         <div>
             <h5>It is {new Date().toLocaleTimeString()}.</h5>
+            <Profiler id ="MouseTracker" onRender= {onRenderCallback}>
             <MouseTracker/>
+            </Profiler>
         </div>
     )
 }
